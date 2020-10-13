a9c1d11737578851587d9012cd91ce3ae0a75f89
samcolson4 <samcolson4@gmail.com>
2020-10-13 16:36:45 +0100

    Moved files to public folder

---
+++ public/src/note-controller.js
@@ -0,0 +1,12 @@
+window.onload = function() {
+  // const heading = document.createElement("h1");
+  // const heading_text = document.createTextNode("Big Head!");
+  // heading.appendChild(heading_text);
+  // document.body.appendChild(heading);
+
+
+  var element = document.getElementById('app')
+  console.log(element)
+  element.innerHTML = 'howdy'
+  
+}
