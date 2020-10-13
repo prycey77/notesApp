4ec9b482a22e1f366c188c37350351194f322cf0
samcolson4 <samcolson4@gmail.com>
2020-10-13 16:39:50 +0100

    Refactor

--- public/src/note-controller.js
+++ public/src/note-controller.js
@@ -1,12 +1,7 @@
 window.onload = function() {
-  // const heading = document.createElement("h1");
-  // const heading_text = document.createTextNode("Big Head!");
-  // heading.appendChild(heading_text);
-  // document.body.appendChild(heading);
 
+    var element = document.getElementById('app')
+    console.log(element)
+    element.innerHTML = 'howdy'
 
-  var element = document.getElementById('app')
-  console.log(element)
-  element.innerHTML = 'howdy'
-  
 }
