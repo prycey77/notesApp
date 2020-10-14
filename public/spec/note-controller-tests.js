0be451cd6656af5e167f4be86f4fb010a420bd87
Richard <rich_pryce@yahoo.co.uk>
2020-10-14 14:36:50 +0100

    Work in progress..

--- public/spec/note-controller-tests.js
+++ public/spec/note-controller-tests.js
@@ -12,18 +12,27 @@ class noteControllerTest {
       "noteController can be instantiated",
       controller instanceof noteController
     );
-    // }
-    // static testInnerHtml() {
-    //   var double = new noteListDouble();
-    //   var controller = new noteController();
-    //   var testDouble = double.test;
-    //   var realTest = controller(testDouble);
-    //   assert.isTrue(
-    //     realTest.view === ["<ul><li><div>Favourite food: pesto</div></li></ul>"]
-    //   );
-    // }
   }
+  //static testInnerHtml() {
+  // var double = new noteListDouble();
+  //var controller = new noteController();
+  //   var testDouble = double.test;
+  //   var realTest = controller(testDouble);
+  // assert.isTrue(
+  //   controller("<ul><li><div>Favourite food: pesto</div></li></ul>"),
+  //   document.getElementById("app")
+  // );
+  // }
+  // }
 }
+
+var noteListVar = new NoteList();
+noteListVar.add("Favourite food: Curry");
+noteListVar.add("Favourite drink: Petrol");
+var noteListViewVar = new noteListView(noteListVar);
+var test = new noteController(noteListViewVar);
+
+test.view();
 
 noteControllerTest.testNoteController();
 // noteControllerTest.testInnerHtml();
--- public/src/note-controller.js
+++ public/src/note-controller.js
@@ -6,9 +6,7 @@ class noteController {
   }
 
   view() {
-    console.log("HELLO");
     window.onload = function () {
-      var element = document.getElementById("app");
       var element = document.getElementById("app");
 
       element.innerHTML = noteListViewVar.output();
