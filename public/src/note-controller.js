b5ecc2619cd7f3c72367c3f64f3283d71e12caae
Richard <rich_pryce@yahoo.co.uk>
2020-10-16 11:52:36 +0100

    Note controller gets the note opject

--- public/src/note-controller.js
+++ public/src/note-controller.js
@@ -12,23 +12,51 @@ class noteController {
     element.innerHTML = this.notelistView.output();
   }
   noteFinder(id) {
-   console.log(this.notelistModel.noteList.reduce(function(note){return note.id == id}))
-     return this.notelistModel.noteList.reduce(function(note){return note.id == id}).text //.filter(a => a.id != 1).text
-     //this.notelistView.noteList.noteList.filter(a => a.id == id).text
-  } 
-  // view() {
-  //   return this.notelistView.output()
-  // }
+    console.log(
+      this.notelistModel.noteList.reduce(function (note) {
+        return note.id == id;
+      })
+    );
+    return this.notelistModel.noteList.reduce(function (note) {
+      return note.id == id;
+    }).text; //.filter(a => a.id != 1).text
+    //this.notelistView.noteList.noteList.filter(a => a.id == id).text
+  }
 
-  // insertOnPage() {
-  //  var element = document.getElementById("app");
-  //  console.log(this)
-  //  element.innerHTML = this.view();
-  // }
+  eventListener() {
+    document.getElementById("text").addEventListener("submit", (e) => {
+      e.preventDefault();
+
+      this.addNoteFunc(e.target.elements[0].value);
+    });
+
+    //   function preventMethod(event) {
+    //     event.preventDefault();
+
+    //     console.log(event.target.elements[0].value);
+    //     addNoteFunc(event.target.elements[0].value);
+    //   }
+  }
+
+  addNoteFunc(note) {
+    console.log(note);
+    this.notelistModel.add(note);
+    console.log(this.notelistModel);
+  }
 }
+
+// view() {
+//   return this.notelistView.output()
+// }
+
+// insertOnPage() {
+//  var element = document.getElementById("app");
+//  console.log(this)
+//  element.innerHTML = this.view();
+// }
 
 var listmodel = new NoteList();
 var controller = new noteController(listmodel);
 controller.view();
+controller.eventListener();
 // controller.noteFinder(1)
- 
